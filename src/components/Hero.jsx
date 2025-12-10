b51ece37ce9054c260b9389b6165fab98ebbb7be
Shariea Reza Nabil <nabilreza183@gmail.com>
2025-12-10 10:58:21 +0600

    add new features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { ChevronDown } from "lucide-react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { codeExamples, floatingCards } from '../data/CodeExample.js';
+import { atelierCaveDark, atomOneDark, nightOwl } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Hero = () => {
 
   const [mousePosition, setMousePosition] = useState({x:0, y:0});
+  const [activeTab, setActiveTab] = useState("App.jsx");
 
   useEffect(() => {
     function handleMouseMove(e) {
@@ -16,6 +21,8 @@ const Hero = () => {
       window.removeEventListener('mousemove', handleMouseMove);
     }
   }, []);
+
+  const currentFloatingCards = floatingCards[activeTab];
 
 
 
@@ -43,10 +50,53 @@ const Hero = () => {
             <ChevronDown className='w-3 h-3 sm:w-4 sm:h-4 text-gray-400'/>
             </div>
 
+            <div className='p-3 sm:p-4 relative h-full'>
+            {/* FILE TABS */}
+            <div className='flex space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto'>
+              <button onClick={() => setActiveTab("App.jsx")} className={`px-3 py-2 backdrop-blur-sm text-xs rounded-t-lg  border ${activeTab === "App.jsx" ? 
+                "bg-blue-500/30 text-white border-blue-400/20" : 
+                "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} text-gray-300 transition-all duration-200 whitespace-nowrap`}>App.jsx</button>
+
+              <button onClick={() => setActiveTab("Hero.jsx")} className={`px-3 py-2 backdrop-blur-sm text-xs rounded-t-lg  border ${activeTab === "Hero.jsx" ? 
+                "bg-blue-500/30 text-white border-blue-400/20" : 
+                "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} text-gray-300 transition-all duration-200 whitespace-nowrap`}>Hero.jsx</button>
+
+              <button onClick={() => setActiveTab("Navber.jsx")} className={`px-3 py-2 backdrop-blur-sm text-xs rounded-t-lg  border ${activeTab === "Navber.jsx" ? 
+                "bg-blue-500/30 text-white border-blue-400/20" : 
+                "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} text-gray-300 transition-all duration-200 whitespace-nowrap`}>Navber.jsx</button>
+            </div>
+
+            {/* Code Content */}
+            <div className='relative overflow-hidden flex-grow'>
+              <SyntaxHighlighter language='javascript' style={atelierCaveDark}
+              customStyle={{
+                            backgroundColor: "#1A1A1A",
+                            margin: 0,
+                            padding: 0,
+                            fontSize: '12px',
+                            borderRadius: '8px',
+                            height: '100%',
+                            border: '1px solid #3c3c3c',
+                          }}
+               showLineNumbers>
+                {codeExamples[activeTab]}
+              </SyntaxHighlighter>
+            </div>
+
           </div>
+          </div>
+
+          {/* FLOATING CARDS */}
+          <div className={`hidden lg:block absolute bottom-4 right-4 transform translate-x-8 translate-y-8 w-72 ${currentFloatingCards.bgColor} backdrop-blur-lg rounded-lg p-4 border border-white/20 shadow-2xl`}>
+            <div className='flex items-center space-x-2 mb-2'>
+              <div className={`w-6 h-6 ${currentFloatingCards.iconColor} flex items-center justify-center text-sm font-bold`}>{currentFloatingCards.icon}</div>
+              <span className={`text-sm font-medium ${currentFloatingCards.textColor}`}>{currentFloatingCards.title}</span>
+            </div>
+            <div className={`text-sm text-left ${currentFloatingCards.contentColor}`}>{currentFloatingCards.context}</div>
+          </div>
+
         </div>
       </div>
-
     </section>
   )
 }
--- src/data/CodeExample.js
+++ src/data/CodeExample.js
@@ -64,3 +64,37 @@ const Navber = () => {
 export default Navber;
 `
 };
+
+
+export const floatingCards = {
+  "App.jsx" : {
+    bgColor: "bg-blue-500/20",
+    iconColor: "text-blue-400",
+    textColor: "text-blue-200",
+    contentColor: "text-blue-300",
+    icon: "AI",
+    title: "Smart Completion",
+    context: "AI-Powred suggestions in real-time"
+  },
+
+  "Hero.jsx" : {
+    bgColor: "bg-purple-500/20",
+    iconColor: "text-purple-400",
+    textColor: "text-purple-200",
+    contentColor: "text-purple-300",
+    icon: "⚡",
+    title: "Auto Animation",
+    context: "Dynamic typing sffects genarated automatically"
+  },
+
+  "Navber.jsx" : {
+    bgColor: "bg-emerald-500/20",
+    iconColor: "text-emerald-400",
+    textColor: "text-emerald-200",
+    contentColor: "text-emerald-300",
+    icon: "🔍",
+    title: "Smart Search",
+    context: "Intelligent code search across your project",
+  },
+
+};
